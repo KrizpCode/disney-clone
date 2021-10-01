06b3a69a828d9c983db6b01c3bf9f1f7c27aca52
KrizpCode <93johan.e@gmail.com>
2021-10-01 07:48:52 +0200

    ViewersVids are done and styled

--- src/components/ImgSlider.tsx
+++ src/components/ImgSlider.tsx
@@ -16,22 +16,22 @@ const ImgSlider = () => {
 		<div>
 			<Carousel {...settings}>
 				<Wrap>
-					<a>
+					<a href="/home">
 						<img src="/images/slider-badging.jpg" alt="" />
 					</a>
 				</Wrap>
 				<Wrap>
-					<a>
+					<a href="/home">
 						<img src="/images/slider-scale.jpg" alt="" />
 					</a>
 				</Wrap>
 				<Wrap>
-					<a>
+					<a href="/home">
 						<img src="/images/slider-badag.jpg" alt="" />
 					</a>
 				</Wrap>
 				<Wrap>
-					<a>
+					<a href="/home">
 						<img src="/images/slider-scales.jpg" alt="" />
 					</a>
 				</Wrap>
